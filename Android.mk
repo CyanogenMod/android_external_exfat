11f589a4b6044ffb146b0e901937d8ec722251ed
Tom Marshall <tdm@cyngn.com>
2014-10-07 23:53:29 -0700

    exfat: Fix symlink generation
     * Don't require inclusion of a separate package for symlinks.  The
       symlinks should always be present if the main package builds.
     * Don't depend on anything for symlink targets.
    
    Change-Id: Ida6e2f1840f8c011e9764da38b26e2b349682f85

--- Android.mk
+++ Android.mk
@@ -12,11 +12,12 @@ LOCAL_SRC_FILES := main.c
 LOCAL_SHARED_LIBRARIES += libz libc
 LOCAL_STATIC_LIBRARIES += libexfat_mount libexfat_fsck libexfat_mkfs
 LOCAL_STATIC_LIBRARIES += libexfat libfuse
+SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,$(LINKS))
+LOCAL_ADDITIONAL_DEPENDENCIES := $(SYMLINKS)
 include $(BUILD_EXECUTABLE)
 
-SYMLINKS := $(addprefix $(TARGET_OUT)/bin/,$(LINKS))
 $(SYMLINKS): EXFAT_BINARY := $(LOCAL_MODULE)
-$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+$(SYMLINKS):
 	@echo "Symlink: $@ -> $(EXFAT_BINARY)"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
@@ -37,11 +38,12 @@ LOCAL_STATIC_LIBRARIES += libz libc
 LOCAL_STATIC_LIBRARIES += libexfat_mount libexfat_fsck libexfat_mkfs
 LOCAL_STATIC_LIBRARIES += libexfat libfuse
 LOCAL_FORCE_STATIC_EXECUTABLE := true
+RECOVERY_SYMLINKS := $(addprefix $(TARGET_RECOVERY_ROOT_OUT)/sbin/,$(LINKS))
+LOCAL_ADDITIONAL_DEPENDENCIES := $(RECOVERY_SYMLINKS)
 include $(BUILD_EXECUTABLE)
 
-RECOVERY_SYMLINKS := $(addprefix $(TARGET_RECOVERY_ROOT_OUT)/sbin/,$(LINKS))
 $(RECOVERY_SYMLINKS): EXFAT_BINARY := mount.exfat
-$(RECOVERY_SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(LOCAL_PATH)/Android.mk
+$(RECOVERY_SYMLINKS):
 	@echo "Symlink: $@ -> $(EXFAT_BINARY)"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
