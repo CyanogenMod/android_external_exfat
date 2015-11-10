34ce2852372390dd0c9ae2d77062b6847f5ccd25
Steve Kondik <steve@cyngn.com>
2015-11-10 14:17:30 +0100

    exfat: Rework build configuration
    Change-Id: Ic13f30258a84593a601a3866ebb55cb4eb82f8a9

--- Android.mk
+++ Android.mk
@@ -1,1 +1,6 @@
-include $(call all-subdir-makefiles)
+FUSE_ROOT := $(call my-dir)
+
+include $(FUSE_ROOT)/libexfat/Android.mk
+include $(FUSE_ROOT)/fuse/Android.mk
+include $(FUSE_ROOT)/mkfs/Android.mk
+include $(FUSE_ROOT)/fsck/Android.mk
--- fuse/Android.mk
+++ fuse/Android.mk
@@ -5,11 +5,21 @@ LOCAL_MODULE := mount.exfat
 LOCAL_MODULE_TAGS := optional
 LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64
 LOCAL_SRC_FILES = main.c 
+LOCAL_SHARED_LIBRARIES := libexfat libfuse
 LOCAL_C_INCLUDES += $(LOCAL_PATH) \
 					external/exfat/libexfat \
 					external/fuse/include \
 					external/fuse/android
-LOCAL_SHARED_LIBRARIES += libz libc libexfat libdl 
-LOCAL_STATIC_LIBRARIES += libfuse
-
 include $(BUILD_EXECUTABLE)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libexfat_mount_static
+LOCAL_MODULE_TAGS := optional
+LOCAL_CFLAGS = -D_FILE_OFFSET_BITS=64 -Dmain=mount_exfat_main
+LOCAL_SRC_FILES = main.c 
+LOCAL_C_INCLUDES += $(LOCAL_PATH) \
+					external/exfat/libexfat \
+					external/fuse/include \
+					external/fuse/android
+LOCAL_STATIC_LIBRARIES := libfuse_static libexfat_static
+include $(BUILD_STATIC_LIBRARY)
