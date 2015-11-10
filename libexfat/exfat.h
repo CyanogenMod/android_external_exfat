b9ffe8e520bb23dfbc57441a0aa505133e70438b
Matt Mower <mowerm@gmail.com>
2015-11-10 14:11:55 -0600

    exfat: ifdef android pre-generated config
    Change-Id: I41a5030ab61eaa3bc960fdac6eaa626d03580044

--- libexfat/exfat.h
+++ libexfat/exfat.h
@@ -24,7 +24,11 @@
 #ifndef EXFAT_H_INCLUDED
 #define EXFAT_H_INCLUDED
 
+#if defined(__ANDROID__)
 #include "android_config.h"
+#else
+#include "config.h"
+#endif
 #include "compiler.h"
 #include "exfatfs.h"
 #include <stdio.h>
